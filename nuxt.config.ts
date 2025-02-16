ac286eef84b2f1339d48262a87dd04d34af89393
William Chong <me@williamchong.cloud>
2025-02-16 20:53:19 +0800

    ➕ Add @nuxt/eslint@1.0.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+  ],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
