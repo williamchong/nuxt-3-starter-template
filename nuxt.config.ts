ebe86bbf860b9a0c316637eac7e75f8de99ba9f8
William Chong <me@williamchong.cloud>
2025-02-17 02:46:09 +0800

    ➕ Add @nuxtjs/i18n@9.2.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,19 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
+    '@nuxtjs/i18n',
     '@nuxtjs/tailwindcss',
   ],
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        language: 'en-US',
+        file: 'en-US.json'
+      },
+    ],
+    defaultLocale: 'en',
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
